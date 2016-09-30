11fba6ba6265d7ef4a36c28b2513db9ab097698d
monicafp <monikfornaris@gmail.com>
2016-09-29 21:39:56 -0400

    Add odometer jquery and corresponding changes to index.html for jquery

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 var express = require('express');
-// var exphbs = require('express-handlebars');
 var app = express();
-// var http = require('http').Server(app);
 
-// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
 app.use(express.static('public'));
-
-// app.get('/', function(req, res){
-//   res.render('./public/index.html');
-// });
-
-// http.listen(3000, function() {
-//   console.log('Server is running.');
-// });
-
-app.post('/calculate', function(request, response) {
-  // Calculations for income
-  // res.json({});
-});
 
 app.listen(3000);
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,33 @@
-// POST jquery to a express route all the values from the form
-$.post(url, formdata, function(data) {
-  $('').append();
-})
+$(document).ready(function() {
+
+  var $totalBudget = $('#total-budget');
+  var $vivienda = $('#vivienda');
+  var $utilidadesServicios = $('#utilidades-servicios');
+  var $transporte = $('#transporte');
+  var $gastosMedicos = $('#gastos-medicos');
+  var $deudasPrestamos = $('#deudas-prestamos');
+  var $comida = $('#comida');
+  var $householdEssentials = $('household-essentials');
+  var $impuestos = $('#impuestos');
+  var $otrosGastos = $('#otros-gastos');
+
+  $otrosGastos.focusout(function() {
+    updateResult();
+  });
+
+  function updateResult() {
+    var result = 0;
+    result = $totalBudget - (
+      ($vivienda.val || 0) +
+      ($utilidadesServicios.val || 0) +
+      ($transporte.val || 0) +
+      ($gastosMedicos.val || 0) +
+      ($deudasPrestamos.val || 0) +
+      ($comida.val || 0) +
+      ($householdEssentials.val || 0) +
+      ($impuestos.val || 0) +
+      ($otrosGastos.val || 0) +
+    );
+    $totalBudget.val(result);
+  };
+});
